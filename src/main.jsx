1eacf1fd020df09f5ffbec55990258ec5ab05ba5
Алексей <115355339+Aleksey-arch@users.noreply.github.com>
2024-12-27 12:12:34 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 import App from './components/App/index.jsx';
 import { BrowserRouter } from 'react-router';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
